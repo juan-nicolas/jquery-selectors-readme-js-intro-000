13ae820cb5f83a2e6e1333a9659e68c7d18bbbc1
Juan Nicolas <jnma91@gmail.com>
2018-10-13 18:06:06 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  
+}
+
+function lastImageSelector() {
+  return $('p')  
+}
+
+function ninjaBabySelector() {
+  
+}
+
+function divSelector() {
+  
+}
+
+function firstListItem(){
+  
+}
